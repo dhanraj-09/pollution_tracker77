1a3ed9b89f5ef85bda4240330465f8e1f5709b9c
Aditya Dhanraj <dhanrajadity201@gmail.com>
2024-10-05 05:58:25 +0530

    aditya

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -4,9 +4,10 @@ import free1 from "../assets/free1.png"
 import video1 from "../assets/video1.mp4"
 import video2 from "../assets/video2.mp4"
 import styled from "styled-components";
-
-
-
+import { gsap } from "gsap/dist/gsap";
+import { CSSRulePlugin } from "gsap/dist/CSSRulePlugin";
+import _gsap from "gsap/gsap-core";
+gsap.registerPlugin(CSSRulePlugin);
 
 
 function Home()
@@ -14,25 +15,35 @@ function Home()
 
     const IMG=styled.img``;
     const VIDEO=styled.video``;
-    const DIV=styled.div``;
+
     const SOURCE=styled.source``;
     return (
         <>
-            <DIV id={"main"}>
+            <div id={"block1"}>
                 <nav id={"nav"}>
                     <IMG src={free1}>
                     </IMG>
-                    <header4>Home</header4>
-                    <header4>Data</header4>
-                    <header4>About</header4>
-                    <header4>Contact Us</header4>
+                    <button>HOME</button>
+                    <button2>Data</button2>
+                    <button3>About</button3>
+                    <button4>Contact Us</button4>
                 </nav>
-                <VIDEO width={"750px"} height={"500px"} autoPlay={true} loop={true}>
-                    <SOURCE src={video1} type={"video/mp4"}></SOURCE>
+                <div className={"video container"}><VIDEO width={"750px"} height={"500px"} autoPlay={true} loop={true}>
+                    <SOURCE src={video2} type={"video/mp4"}></SOURCE>
                 </VIDEO>
-            </DIV>
+                    <div className={"header-overlay"}>
+                        <h1>KNOW.YOUR.AIR</h1>
+                        <h2>WELCOME TO BREATHE SAFE</h2>
+                        <p>"Tracking Air Quality for a Healthier you"</p>
+                    </div>
+                </div>
+
+
+            </div>
+
+
         </>
-            )
-            }
+    )
+}
 
 export default Home
